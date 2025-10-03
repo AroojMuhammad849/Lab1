ec32b63f11cd177f4665d9dbaa8661477834896b
AroojMuhammad849 <Aroojmuhammad849@gmail.com>
2025-10-03 15:45:51 +0500

    Lab 2 variables and i/o

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,35 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Hello, World!"<<endl;
+//     return 0;
+// }
+
+#include <iostream>
+#include <string>
 using namespace std;
-int main(){
-    cout<<"Hello, World!"<<endl;
+
+int main() {
+    string name;
+    int age;
+    float gpa;
+
+    cout << "Enter name: ";
+    cin >> name;
+
+    cout << "Enter age: ";
+    cin >> age;
+
+    cout << "Enter GPA: ";
+    cin >> gpa;
+
+    cout << "Student: " << name 
+         << ", Age: " << age 
+         << ", GPA: " << gpa << endl;
+
     return 0;
 }
+
+
  
 
